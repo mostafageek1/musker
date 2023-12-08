5e53eab2baddbeca5e69253e886f83f813909e77
Mostafa Ibnelkhattab <mostafageek1@gmail.com>
2023-12-08 21:28:38 +0100

    error fix

--- social/settings.py
+++ social/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse('database_url')
+database_url = os.environ.get('database_url')
+DATABASES = {'default': dj_database_url.parse('database_url')}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
